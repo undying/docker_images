e50a1c5aff22fe515cf5cad412b5604fec051b01
Denis Bozhok <kron@ostrovok.ru>
2016-12-05 03:13:39 +0300

    +busybox x86_64

--- Makefile
+++ Makefile
@@ -9,4 +9,9 @@ busybox-i686: busybox-i686-1.21.1
 busybox-i686-1.21.1:
 	opt/build.sh -d busybox -r 1.21.1 -a i686
 
-.PHONY: debian-i386 debian-i386-jessie busybox-i686 busybox-i686-1.21.1
+busybox-x86_64: busybox-x86_64-1.21.1
+
+busybox-x86_64-1.21.1:
+	opt/build.sh -d busybox -r 1.21.1 -a x86_64
+
+.PHONY: debian-i386 debian-i386-jessie busybox-i686 busybox-i686-1.21.1 busybox-x86_64 busybox-x86_64-1.21.1
--- opt/modules/busybox.sh
+++ opt/modules/busybox.sh
@@ -3,11 +3,28 @@
 opkg_version=0.2.4
 cpu_count=$(grep -c processor /proc/cpuinfo)
 
-mkdir -p ${chroot_path}/{bin,dev,etc,lib,proc,sbin,sys,usr,tmp}
-mkdir -p ${chroot_path}/etc/{init.d,opkg}
-mkdir -p ${chroot_path}/usr/{bin,sbin,lib,opkg} ${chroot_path}/usr/lib/opkg
-mkdir -p ${chroot_path}/var/log
-touch ${chroot_path}/etc/rc.common
+case ${arch} in
+  x86_64)
+    opkg_arch=${arch}
+    opkg_mirrors="
+src/gz packages http://downloads.openwrt.org/snapshots/trunk/x86/64/packages/packages
+src/gz base http://downloads.openwrt.org/snapshots/trunk/x86/64/packages/base"
+    opkg_libc_url="http://downloads.openwrt.org/snapshots/trunk/x86/64/packages/base/libc_1.1.15-1_x86_64.ipk"
+    ;;
+  *)
+    opkg_arch=x86
+    opkg_mirrors="
+src/gz packages http://downloads.openwrt.org/snapshots/trunk/x86/generic/packages/packages
+src/gz base http://downloads.openwrt.org/snapshots/trunk/x86/generic/packages/base
+    "
+    opkg_libc_url="http://downloads.openwrt.org/snapshots/trunk/x86/generic/packages/base/libc_1.1.15-1_x86.ipk"
+esac
+
+
+mkdir -pv ${chroot_path}/{bin,dev,etc,lib,proc,sbin,sys,usr,tmp}
+mkdir -pv ${chroot_path}/etc/{init.d,opkg}
+mkdir -pv ${chroot_path}/usr/{bin,sbin,lib,opkg} ${chroot_path}/usr/lib/opkg
+mkdir -pv ${chroot_path}/var/log
 
 
 wget -O ${chroot_path}/sbin/busybox \
@@ -21,8 +38,13 @@ rm -v ${chroot_path}/linuxrc
 wget -O /tmp/opkg-${opkg_version}.tar.gz \
   https://git.yoctoproject.org/cgit/cgit.cgi/opkg/snapshot/opkg-${opkg_version}.tar.gz
 
-wget -O ${chroot_path}/lib/functions.sh \
-  'https://dev.openwrt.org/browser/trunk/package/base-files/files/lib/functions.sh?format=txt'
+for f in etc/rc.common lib/functions.sh lib/functions/system.sh lib/functions/service.sh;do
+  out=${chroot_path}/${f}
+  mkdir -pv $(dirname ${out})
+
+  wget -O ${out} \
+    "https://dev.openwrt.org/browser/trunk/package/base-files/files/${f}?format=txt"
+done
 
 
 tar -xzf /tmp/opkg-${opkg_version}.tar.gz -C /tmp
@@ -45,10 +67,8 @@ cd ${chroot_path}/bin
 ln -s opkg-cl opkg
 
 
-cat - > ${chroot_path}/etc/opkg/opkg.conf <<EOF
-src/gz packages http://downloads.openwrt.org/snapshots/trunk/x86/generic/packages/packages
-src/gz base http://downloads.openwrt.org/snapshots/trunk/x86/generic/packages/base/
-
+echo -e "${opkg_mirrors}\n" > ${chroot_path}/etc/opkg/opkg.conf
+cat - >> ${chroot_path}/etc/opkg/opkg.conf <<EOF
 lists_dir packages /usr/lib/opkg/lists
 lists_dir base /usr/lib/opkg/lists
 EOF
@@ -56,15 +76,14 @@ EOF
 cat - > ${chroot_path}/etc/opkg/arch.conf <<EOF
 arch all 1
 arch noarch 1
-arch x86 2
-arch i686 5
-arch i386 10
+arch ${opkg_arch} 2
 EOF
 
 
 cp -v /etc/resolv.conf ${chroot_path}/etc/
 chroot ${chroot_path} opkg update
-chroot ${chroot_path} opkg install http://downloads.openwrt.org/snapshots/trunk/x86/generic/packages/base/libc_1.1.15-1_x86.ipk
+chroot ${chroot_path} opkg install ${opkg_libc_url}
+
 
 for f in ${chroot_path}/etc/resolv.conf ${chroot_path}/usr/lib/opkg/lists/*;do
   rm -v ${f}
